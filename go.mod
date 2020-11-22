4958832ed19b083b1080cb2044023b0e718d9b03
Roy <roy.motloutsi@gmail.com>
2020-11-22 17:35:40 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/emelent/go-with-tests
+
+go 1.15
+
+require github.com/jstemmer/gotags v1.4.1 // indirect
---
+++ helloworld/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+// Hello greets
+func Hello(name string) string {
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
