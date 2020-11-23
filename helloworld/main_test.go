76b03802d9c1f1ecf2d10a31d3304f90c99facad
Roy <roy.motloutsi@gmail.com>
2020-11-23 12:56:10 +0200

    feat(Hello): greet in French

--- helloworld/main_test.go
+++ helloworld/main_test.go
@@ -29,4 +29,11 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
