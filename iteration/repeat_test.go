14dae33d5cd245886ca679d55d9aaf26b7f71391
Roy <roy.motloutsi@gmail.com>
2020-11-27 18:13:21 +0200

    feat(repeat): accept count parameter  on Repeat function

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,10 +3,26 @@ package iteration
 import "testing"
 
 func TestRepeating(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat a, 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	t.Run("repeat c, 8 times", func(t *testing.T) {
+		repeated := Repeat("c", 8)
+		expected := "cccccccc"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 3)
 	}
 }
