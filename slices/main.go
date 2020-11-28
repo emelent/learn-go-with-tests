a95718a7e93ae9b7e99cf89774a849c89d50417d
Roy <roy.motloutsi@gmail.com>
2020-11-28 14:48:49 +0200

    feat(sum_all): implement SumAll

--- slices/main.go
+++ slices/main.go
@@ -1,5 +1,6 @@
 package main
 
+// Sum returns the sum of a slice of integers
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
@@ -8,10 +9,19 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(sets ...[]int) int {
+// SumAll2 returns the sum of a set of slices
+func SumAll2(sets ...[]int) int {
 	sum := 0
 	for _, set := range sets {
 		sum += Sum(set)
 	}
 	return sum
+}
+
+// SumAll returns a slice containing the individual sums of a set of slices
+func SumAll(sets ...[]int) (sums []int) {
+	for _, set := range sets {
+		sums = append(sums, Sum(set))
+	}
+	return
 }
--- slices/main_test.go
+++ slices/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -16,15 +19,24 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func TestSumAll(t *testing.T) {
+func TestSumAll2(t *testing.T) {
 	setA := []int{1, 2, 3}
 	setB := []int{4, 5, 6}
 
-	got := SumAll(setA, setB)
+	got := SumAll2(setA, setB)
 	want := 21
 
 	if got != want {
 		t.Errorf("got %d want %d given, (%v, %v)", got, want, setA, setB)
 	}
 
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
