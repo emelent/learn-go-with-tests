5d9816e6a9200094df4c40d00a58e828a2bb85fc
Roy <roy.motloutsi@gmail.com>
2020-11-29 18:13:18 +0200

    feat(pointers/wallet_test): update wallet test

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -14,10 +14,14 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertError := func(t *testing.T, err error) {
+	assertError := func(t *testing.T, got error, want string) {
 		t.Helper()
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
@@ -35,10 +39,10 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
-		wallet := Wallet{balance: startingBalance}
+		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-		assertError(t, err)
+		assertError(t, err, "cannot withdraw, insufficient funds")
 	})
 }
