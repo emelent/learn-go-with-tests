c7c14c256cac887f5d2e137e8ca4f3480f41a272
Roy <roy.motloutsi@gmail.com>
2020-11-30 22:00:54 +0200

    feat(maps): refactor

--- maps/main.go
+++ maps/main.go
@@ -2,12 +2,14 @@ package main
 
 import "errors"
 
+var ErrNotFound = errors.New("word not found")
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
 	def, ok := d[word]
 	if !ok {
-		return "", errors.New("word not found")
+		return "", ErrNotFound
 	}
 	return def, nil
 }
