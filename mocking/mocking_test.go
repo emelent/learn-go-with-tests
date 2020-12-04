ce683f8b8c659e09a647249b8557f9c1a3b2d952
Roy <roy.motloutsi@gmail.com>
2020-12-05 00:15:13 +0200

    feat(mocking/mocking_test): add test

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -13,20 +14,59 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type CountdownOperationSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	sleeper := &SpySleeper{}
 
-	Countdown(buffer, sleeper)
+	t.Run("prints correct output", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		sleeper := &SpySleeper{}
 
-	got := buffer.String()
-	want := "3\n2\n1\nGo!\n"
+		Countdown(buffer, sleeper)
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		got := buffer.String()
+		want := "3\n2\n1\nGo!\n"
 
-	if sleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", sleeper.Calls)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+		if sleeper.Calls != 4 {
+			t.Errorf("not enough calls to sleeper, want 4 got %d", sleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
