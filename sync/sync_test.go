ecccb45733b5059bc6d368b7a7a81dce3a911849
Roy <roy.motloutsi@gmail.com>
2020-12-09 20:10:28 +0200

    feat(sync): use mutex to meet spec

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -11,11 +14,29 @@ func TestCounter(t *testing.T) {
 
 		assertCounter(t, counter, 3)
 	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
 func assertCounter(t *testing.T, counter Counter, want int) {
 	t.Helper()
-	if got := counter.Value(); got != 3 {
+	if got := counter.Value(); got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
