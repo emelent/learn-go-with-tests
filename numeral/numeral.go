8dac69fc1ec90ef8545b19eaa0be0d2a6320bffa
Roy <roy.motloutsi@gmail.com>
2020-12-20 18:25:55 +0200

    feat(numeral): complete numeral sample

--- numeral/numeral.go
+++ numeral/numeral.go
@@ -3,13 +3,13 @@ package numeral
 import "strings"
 
 type romanNumeral struct {
-	Value  int
+	Value  uint16
 	Symbol string
 }
 
 type romanNumerals []romanNumeral
 
-func (r romanNumerals) ValueOf(symbols ...byte) int {
+func (r romanNumerals) ValueOf(symbols ...byte) uint16 {
 	symbol := string(symbols)
 	for _, s := range r {
 		if s.Symbol == symbol {
@@ -63,7 +63,7 @@ func (w windowedRoman) Symbols() (symbols [][]byte) {
 	return
 }
 
-func ConvertToRoman(arabic int) string {
+func ConvertToRoman(arabic uint16) string {
 	var result strings.Builder
 
 	for _, numeral := range allRomanNumerals {
@@ -76,7 +76,7 @@ func ConvertToRoman(arabic int) string {
 	return result.String()
 }
 
-func ConvertToArabic(roman string) (total int) {
+func ConvertToArabic(roman string) (total uint16) {
 	for _, symbols := range windowedRoman(roman).Symbols() {
 		total += allRomanNumerals.ValueOf(symbols...)
 	}
