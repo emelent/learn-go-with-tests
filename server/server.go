465002469e2941e6fd93fe6406689bc783b0f884
Roy <roy.motloutsi@gmail.com>
2020-12-21 22:14:30 +0200

    feat(server): sliming it

--- server/server.go
+++ server/server.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+	}
+}
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	log.Printf("Serving on 0.0.0.0:5000\n\n")
+	log.Fatal(http.ListenAndServe(":5000", handler))
 }
